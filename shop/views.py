8c63ce70d0a5fcd3e19636b270f102cf6de3cd28
kartikey <kartikeym1@gmail.com>
2023-02-26 20:23:15 +0530

    Day-10

--- shop/views.py
+++ shop/views.py
@@ -8,12 +8,19 @@ from django.http import HttpResponse
 def index(request):
     products = Product.objects.all()
     print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
+   # n = len(products)
+   # nSlides = n//4 + ceil((n/4)-(n//4))
    # params = {'no_of_slides':nSlides,'range':range(1,nSlides),'product': products}
-    allProds = [[products, range(1,nSlides), nSlides],
-               [products, range(1,nSlides), nSlides]]
-           
+    #allProds = [[products, range(1,nSlides), nSlides],
+     #          [products, range(1,nSlides), nSlides]]
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods} 
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(products)
+        nSlides = n//4 + ceil((n/4)-(n//4))
+        allProds.append([prod, range(1,nSlides),nSlides])      
     params= {'allProds':allProds}
     return render (request, 'shop/index.html', params)
 def about(request):
