6ef59b6515adc32174db1b22ac7d1c31b103276a
kartikey <kartikeym1@gmail.com>
2023-03-05 13:22:09 +0530

    Day-20

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-     path("",views.index, name="shopHome")
+     path("",views.index, name="blogHome"),
+     path("blogpost/",views.blogpost, name="blogPost")
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 def index(request):
     return render (request, 'blog/index.html')
 
-    
+def blogpost(request):
+    return render (request, 'blog/blogpost.html')
