784665606abc0af159eadb4d760278207b3e1795
kartikey <kartikeym1@gmail.com>
2023-03-08 20:46:19 +0530

    Day-23

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-     path("",views.index, name="shopname"),
-     path("about/",views.about, name="About Us"),
-     path("contact/",views.contact, name="Contact Us"),
-     path("tracker/",views.tracker, name="TrackingStatus"),
-     path("search/",views.search, name="Search"),
-     path("products/<int:myid>",views.productView, name="productview"),
-     path("checkout/",views.checkout, name="Checkout"),
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="Checkout"),
+    path("handlerequest/", views.handlerequest, name="HandleRequest"),
 
 ]
