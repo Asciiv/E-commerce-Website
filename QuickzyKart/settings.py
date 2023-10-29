482be5b491939870fde8244e198eee54f3427e26
Asciiv <110736665+Asciiv@users.noreply.github.com>
2023-10-29 18:12:05 +0530

    Update settings.py

--- QuickzyKart/settings.py
+++ QuickzyKart/settings.py
@@ -139,13 +139,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-razorpay_id = 'rzp_test_WlFGAwY29dwISU'
-razorpay_account_id = '0FPr7Fm1tXD20U07U30oru9V'
+razorpay_id = ''
+razorpay_account_id = ''
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kartikeym1@gmail.com'
-EMAIL_HOST_PASSWORD = 'vjmiwrhmjjqatsut'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
