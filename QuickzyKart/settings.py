68a898df28cdd39d064e25a1c7fa3c292dc16af0
Asciiv <110736665+Asciiv@users.noreply.github.com>
2023-10-29 18:09:50 +0530

    Update settings.py

--- QuickzyKart/settings.py
+++ QuickzyKart/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kartikeym1@gmail.com'
-EMAIL_HOST_PASSWORD = 'vjmiwrhmjjqatsut'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
